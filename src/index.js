9d9fb7efc4e0bb73e6d8f97501e5e5123b62bd1d
Yulia Kravtsova <jajajajajaja1999@gmail.com>
2019-10-08 18:57:18 +0300

    Finished the task

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      let hours;
+      if (knowsProgramming === true) {
+        hours = 800
+      } else {
+        hours = 1300
+      }
+      return Math.ceil(hours/config[focus]);
   };
   
